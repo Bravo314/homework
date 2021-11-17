e680b8d65ad3f2b6ede7f82318b21dc3fe8c9c6c
Johnny Word <word314@gmail.com>
2021-11-17 16:25:39 -0600

    correct hw

--- Week2.js
+++ Week2.js
@@ -1,5 +1,5 @@
 //create two variables: pizzaPlace and numberOfToppings
-let pizzaPlace = wordsPizza;
+let pizzaPlace = "wordsPizza";
 
 console.log(typeof pizzaPlace);
 
@@ -7,13 +7,13 @@ let numberOfToppings = 10;
 
 console.log(typeof numberOfToppings);
 //Print a template literal that uses both variables to construct a short sentence about your Pizza Place.
-`"hello wordsPizza", I said, "I would like 10 numbeOfToppings."`
+console.log(`"hello ${pizzaPlace}", I said, "I would like ${numberOfToppings}."`)
 
 //Construct an if statement that prints "Quality, not quantity." if you offer less than 10 toppings, or "A whole lot of pizza." if you offer 10 or more toppings.
 
-if numberOfToppings < 10 {
+if (numberOfToppings < 10) {
   console.log("Quality, not quanity.");
-} else if numberOfToppings > 10 {
+} else {
   console.log("A whole lot of pizza")
 }
 
